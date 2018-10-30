ddf65f8acdcff5f9b20dcbf593c52ceb314a5499
Golehm <grzegorz.gorski@yahoo.pl>
2018-10-30 21:14:46 +0100

    commit challenge8

---
+++ challenges8.sql
@@ -0,0 +1,20 @@
+SELECT title FROM books WHERE title LIKE '%stories%';
+
+SELECT title, MAX(pages) AS pages FROM books;
+
+SELECT CONCAT(title, ' - ', released_year) AS summary 
+FROM books ORDER BY released_year DESC LIMIT 3;
+
+SELECT title, author_lname FROM books WHERE author_lname LIKE '% %';
+
+SELECT title, released_year, stock_quantity 
+FROM books ORDER BY stock_quantity LIMIT 3;
+
+SELECT title, author_lname FROM books ORDER BY author_lname, title;
+
+SELECT 
+    CONCAT(
+        'MY FAVOURITE AUTHOR IS ',
+        UPPER(CONCAT(author_fname, ' ',author_lname, '!'))
+    ) AS yell
+FROM books ORDER BY author_lname;
